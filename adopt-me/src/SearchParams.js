d4f498c350ea2e8fc9ec0ed9c35715732dd6c91e
craigcliff <craigcliff@gmail.com>
2022-07-24 08:56:17 +0200

    Added useState example

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 const SearchParams = () => {
-  const location = "Cape Town";
+  //  Dont add hooks inside if of for statements!!!!
+  const [location, setLocation] = useState("");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
