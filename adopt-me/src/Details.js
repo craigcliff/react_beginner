5d04c8f2c0669c81d2569bb94554e04209e833bc
craigcliff <craigcliff@gmail.com>
2022-08-12 13:14:47 +0200

    Fetching using componentDidMount

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -9,6 +9,16 @@ class Details extends Component {
     this.state = { loading: true };
   }
 
+  //coponentDidMount similar to useEffect
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
   render() {
     if (this.state.loading) {
       return <h2>loading ... </h2>;
@@ -31,4 +41,10 @@ class Details extends Component {
   }
 }
 
-export default Details;
+// to use hooks with class component, you need to wrap class component in function component.
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
