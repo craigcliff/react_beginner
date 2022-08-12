e4feb398fc5ada222347842417b34c0fe82c2902
craigcliff <craigcliff@gmail.com>
2022-08-12 12:18:40 +0200

    Adding React router

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+
 const App = () => {
   return (
-    <div id="my-app">
-      <h1>Adopt Me</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <h1>Adopy Me Please!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
